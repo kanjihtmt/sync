f5518e7c3db34c2cdd5c67485fb01254ec45a87b
kanji <kanjihtmt@gmail.com>
2016-06-07 05:30:35 +0900

    has_secure_passwordに合わせてパスワードのカラム名を変更

---
+++ db/migrate/20160606202359_modifiy_password_column.rb
@@ -0,0 +1,5 @@
+class ModifiyPasswordColumn < ActiveRecord::Migration
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606133908) do
+ActiveRecord::Schema.define(version: 20160606202359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,10 +46,10 @@ ActiveRecord::Schema.define(version: 20160606133908) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "telephone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
