80c41d4c837601f89177e3e9263aed6e7809fd94
kanji <kanjihtmt@gmail.com>
2016-06-07 05:07:57 +0900

    ログイン認証の為のユーザモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605165417) do
+ActiveRecord::Schema.define(version: 20160606133908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20160605165417) do
     t.integer  "retailer_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
+    t.string   "telephone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
