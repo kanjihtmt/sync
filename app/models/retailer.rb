4f0309fd32ffd75edb6dcc234997dc0ddf2a90d3
kanji <kanjihtmt@gmail.com>
2016-06-24 01:54:01 +0900

    関連テーブルのステータスも更新するように修正

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -2,4 +2,16 @@ class Retailer < ActiveRecord::Base
   include Statusable
 
   has_many :sales
+  #
+  # accepts_nested_attributes_for :sales
+
+  after_save do
+    if self.status == Retailer::DELETED
+      self.sales.each do |sale|
+        sale.status = Sale::DELETED
+        sale.save!
+      end
+    end
+  end
 end
+
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,4 +3,13 @@ class Sale < ActiveRecord::Base
 
   belongs_to :retailer
   has_many :payments
+
+  after_save do
+    if self.status == DELETED
+      self.payments.each do |payment|
+        payment.status = DELETED
+        payment.save!
+      end
+    end
+  end
 end
